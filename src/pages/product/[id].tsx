e58d3a436e20d8ee6924e225579253148f10e38b
Guilherme Almeida <gui.almeidacrz@gmail.com>
2023-08-21 21:34:09 -0300

    feat: stripe checkout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,6 +12,7 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
@@ -68,6 +69,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1, // 1 hour
